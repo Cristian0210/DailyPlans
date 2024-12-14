4d6837532e3e3f0497a68cce20b4776946def05b
Cristian <vangheli.cristian22@gmail.com>
2024-12-14 20:55:24 +0100

    first commit

---
+++ Planes diarios/Planes_diariosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Planes_diariosApp.swift
+//  Planes diarios
+//
+//  Created by Cristian Vangheli on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Planes_diariosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
